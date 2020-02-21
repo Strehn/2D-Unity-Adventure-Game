237ae9cdbd23c6682ca0ae3d8ed2837637b56f28
Taegan Williams <taeganw@gmail.com>
2020-02-21 00:18:05 -0800

    fixed the audio script to play random walk audio on key press

--- Existential/Assets/SoundScript.cs
+++ Existential/Assets/SoundScript.cs
@@ -17,64 +17,70 @@ public class SoundScript : MonoBehaviour
     private bool step = true;
     float audioStepLengthWalk = 0.45f;
     float audioStepLengthRun = 0.25f;
-
+    AudioSource MyAudioSource;
     // Start is called before the first frame update
     void Start()
     {
-
+        var audioClip = Resources.Load<AudioClip>("Assets/6-Taegan/audio/snow.wav");
+        MyAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(moveUp) || Input.GetKey(moveDown) || Input.GetKey(moveLeft) || Input.GetKey(moveRight))
+        if (Input.GetKeyDown(moveUp) || Input.GetKeyDown(moveDown) || Input.GetKeyDown(moveLeft) || Input.GetKeyDown(moveRight))
         {
-            if (hit.gameObject.tag == "Wood" && step == true && controller.velocity.magnitude < 7)
-            {
-                WalkOnWood();
-            }
-            else if (hit.gameObject.tag == "Wood" && step == true && controller.velocity.magnitude > 7)
-            {
-                RunOnWood();
-            }
-            else if (hit.gameObject.tag == "Grass" && step == true && controller.velocity.magnitude < 7)
-            {
-                WalkOnGrass();
-            }
-            else if (hit.gameObject.tag == "Grass" && step == true && controller.velocity.magnitude > 7)
-            {
-                RunOnGrass();
-            }
-            else if (hit.gameObject.tag == "Snow" && step == true && controller.velocity.magnitude < 7)
-            {
-                WalkOnSnow();
-            }
-            else if (hit.gameObject.tag == "Snow" && step == true && controller.velocity.magnitude > 7)
-            {
-                RunOnSnow();
-            }
-            else if (hit.gameObject.tag == "Forest" && step == true && controller.velocity.magnitude < 7)
-            {
-                WalkOnForest();
-            }
-            else if (hit.gameObject.tag == "Forest" && step == true && controller.velocity.magnitude > 7)
-            {
-                RunOnForest();
-            }
-            else if (hit.gameObject.tag == "Town" && step == true && controller.velocity.magnitude < 7)
-            {
-                WalkOnTown();
-            }
-            else if (hit.gameObject.tag == "Town" && step == true && controller.velocity.magnitude > 7)
-            {
-                RunOnTown();
-            }
+            //if (GameObject.FindGameObjectsWithTag("Snow") && step == true && controller.velocity.magnitude < 7)
+            //{
+            //    // WalkOnWood();
+            //}
+            //else if (hit.gameObject.tag == "Wood" && step == true && controller.velocity.magnitude > 7)
+            //{
+            //    // RunOnWood();
+            //}
+            //else if (hit.gameObject.tag == "Grass" && step == true && controller.velocity.magnitude < 7)
+            //{
+            //    // WalkOnGrass();
+            //}
+            //else if (hit.gameObject.tag == "Grass" && step == true && controller.velocity.magnitude > 7)
+            //{
+            //    //RunOnGrass();
+            //}
+            //else if (hit.gameObject.tag == "Snow" && step == true && controller.velocity.magnitude < 7)
+            //{
+            //    // WalkOnSnow();
+            //}
+            //else if (hit.gameObject.tag == "Snow" && step == true && controller.velocity.magnitude > 7)
+            //{
+            //    //RunOnSnow();
+            //}
+            //else if (hit.gameObject.tag == "Forest" && step == true && controller.velocity.magnitude < 7)
+            //{
+            //    // WalkOnForest();
+            //}
+            //else if (hit.gameObject.tag == "Forest" && step == true && controller.velocity.magnitude > 7)
+            //{
+            //    //RunOnForest();
+            //}
+            //else if (hit.gameObject.tag == "Town" && step == true && controller.velocity.magnitude < 7)
+            //{
+            //    // WalkOnTown();
+            //}
+            //else if (hit.gameObject.tag == "Town" && step == true && controller.velocity.magnitude > 7)
+            //{
+            //    RunOnTown();
+            //}
+            RunOnTown();
+        }
+        else if (Input.GetKeyUp(moveUp) || Input.GetKeyUp(moveDown) || Input.GetKeyUp(moveLeft) || Input.GetKeyUp(moveRight))
+        {
+            GetComponent<AudioSource>().Stop();
         }
     }
 
     void WalkOnTown()
     {
-        GetComponent<AudioSource>().clip = concrete[Random.Range(0, concrete.Length)];
+        GetComponent<AudioSource>().clip = snow[Random.Range(0, snow.Length)];
         GetComponent<AudioSource>().volume = 0.1f;
         GetComponent<AudioSource>().Play();
         StartCoroutine(WaitForFootSteps(audioStepLengthWalk));
@@ -82,11 +88,17 @@ public class SoundScript : MonoBehaviour
 
     void RunOnTown()
     {
-        GetComponent<AudioSource>().clip = concrete[Random.Range(0, concrete.Length)];
-        GetComponent<AudioSource>().volume = 0.1f;
+        MyAudioSource.clip = snow[Random.Range(0, snow.Length)];
+        MyAudioSource.volume = 0.1f;
         GetComponent<AudioSource>().Play();
-        StartCoroutine(WaitForFootSteps(audioStepLengthWalk));
+        //StartCoroutine(WaitForFootSteps(audioStepLengthWalk));
+    }
+
+    IEnumerator WaitForFootSteps(float stepsLength)
+    {
+        step = false;
+        yield return new WaitForSeconds(stepsLength);
+        step = true;
     }
 
 }
-
