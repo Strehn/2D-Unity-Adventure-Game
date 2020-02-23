377616848c06896090ce7dc5dd754457af051360
Sydney Petrehn <Sydneypetrehn!@gmail.com>
2020-02-23 13:43:09 -0800

    Added files
    Scenes for every person

--- Existential/Assets/5-Ronnie/Filler.cs
+++ Existential/Assets/5-Ronnie/Filler.cs
@@ -1,8 +1,8 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Filler : MonoBehaviour
+public class Filler2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
