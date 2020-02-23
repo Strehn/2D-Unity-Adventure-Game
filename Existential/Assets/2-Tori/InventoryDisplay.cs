cd33cdbf2aa20c8bd5cc55835e527c751e30f2f7
Victoria Gehring <victoria.gehring@gmail.com>
2020-02-22 16:56:11 -0800

    inventory button/scene and running total

---
+++ Existential/Assets/2-Tori/InventoryDisplay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryDisplay : MonoBehaviour
+{
+
+    private int inventory = 0;
+    public Text inventoryText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        inventoryText.text = "INVENTORY : " + inventory;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            inventory++;
+        }
+    }
+}
---
+++ Existential/Assets/2-Tori/InventoryMenuButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InventoryMenuButton : MonoBehaviour
+{ 
+    public void OnMouseDown()
+    {
+        Debug.Log("Going to inventory screen");
+        SceneManager.LoadScene(1);
+        // Will add scene later SceneManager.LoadScene();
+    }
+
+}
