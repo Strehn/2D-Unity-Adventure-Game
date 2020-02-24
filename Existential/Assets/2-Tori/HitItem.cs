4ba8d95d49dc055d7c8af832fa7ca88383c536eb
Victoria Gehring <victoria.gehring@gmail.com>
2020-02-23 17:32:27 -0800

    temp delete of buttons

---
+++ Existential/Assets/2-Tori/HitItem.cs
@@ -0,0 +1,31 @@
+﻿/* using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitItem : MonoBehaviour
+{
+    Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = cam.SreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.name == "apple")
+            {
+                Debug.Log("hit apple!!!");
+            }
+        }
+
+        
+    }
+}
+*/
