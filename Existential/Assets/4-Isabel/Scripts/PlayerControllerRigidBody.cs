564ba553093df542fec65065acff0c52eed52fe4
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-02-23 17:38:48 -0800

    Attached player movement to main character

---
+++ Existential/Assets/4-Isabel/Scripts/PlayerControllerRigidBody.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Isabel
+
+public class PlayerControllerRigidBody : MonoBehaviour{
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // Get input from Horizontal and Vertical Axes
+        moveVelocity = moveInput.normalized * speed;  // Move according to speed
+    }
+    void FixedUpdate(){
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);  // Move as long as the arrow keys are pressed
+    }
+
+}
