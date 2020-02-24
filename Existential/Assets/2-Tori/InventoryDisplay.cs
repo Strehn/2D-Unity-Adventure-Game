c87a429fe91f599c1e0a63b3907044c2f07ffa52
Victoria Gehring <victoria.gehring@gmail.com>
2020-02-23 19:31:53 -0800

    mouse over inv item will disappear

--- Existential/Assets/2-Tori/InventoryDisplay.cs
+++ Existential/Assets/2-Tori/InventoryDisplay.cs
@@ -7,27 +7,36 @@ public class InventoryDisplay : MonoBehaviour
 {
     public int inventory = 0;
     public Text inventoryText;
+    public Camera cam;
+    bool ishit = false;
     // Start is called before the first frame update
     void Start()
     {
         GameObject gameObjectInv = GameObject.Find("InventoryTotal");
         inventoryText = gameObjectInv.GetComponent<Text>();
-
+        cam = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     public void Update()
     {
+        
         inventoryText.text = "INVENTORY : " + inventory;
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             inventory++;
         }
+        if (hit.collider != null)
+        {
+            Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+            
+            ishit = true;
+            Debug.Log(hit.collider.gameObject);
+            hit.collider.gameObject.SetActive(false);
+            inventory++;
+        }
+        
     }
 
-    public void additem()
-    {
-        inventory++;
-        this.Update();
-    }
 }
