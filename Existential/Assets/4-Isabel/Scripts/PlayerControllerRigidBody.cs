da755e0b33102856ed81d29432915a42c0ae9dc2
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-02-25 13:57:17 -0800

    Fixed script

--- Existential/Assets/4-Isabel/Scripts/PlayerControllerRigidBody.cs
+++ Existential/Assets/4-Isabel/Scripts/PlayerControllerRigidBody.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 // Isabel
 
 public class PlayerControllerRigidBody : MonoBehaviour{
-    public float speed;
-    private Rigidbody2D rb;
-    private Vector2 moveVelocity;
+    public float speed;  // Variable for speed movement (should be about 2)
+    private Rigidbody2D rb;  // Rigidbody2D for 2D character movement
+    private Vector2 moveVelocity;  // Vector for how fast the player will move on screen
 
     void Start(){
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();  // Get the Rigidbody2D from the character components 
     }
 
     // Update is called once per frame
@@ -18,6 +18,7 @@ public class PlayerControllerRigidBody : MonoBehaviour{
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")); // Get input from Horizontal and Vertical Axes
         moveVelocity = moveInput.normalized * speed;  // Move according to speed
     }
+    // FixedUpdate runs independently per frame (can be zero, one, or multiple)
     void FixedUpdate(){
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);  // Move as long as the arrow keys are pressed
     }
