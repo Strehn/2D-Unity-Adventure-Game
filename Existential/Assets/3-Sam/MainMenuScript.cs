d0d1f37d992fd9876c125b13dd6d383bf0c50251
samilynn13 <60374710+samilynn13@users.noreply.github.com>
2020-02-25 12:12:23 -0800

    Added Main Menu Scene

---
+++ Existential/Assets/3-Sam/MainMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour{
+
+    //Function for NewGameButton
+    public void PlayNewGame() {
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Function for QuitGameButton
+    public void QuitGame() {
+
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
