165ab95d3f2e8d0a5395356cdf213f04dc15a3f0
Ronald Keating <rvnnie@yahoo.com>
2020-03-08 19:12:26 -0700

    Added snow

---
+++ Existential/Assets/5-Ronnie/Scripts/SnowParticles.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowParticles : MonoBehaviour
+{
+    public GameObject snow;
+    private float rand;
+    private int poss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 28; i++)
+        {
+            poss = Random.Range(1, 15);
+            if (poss == 1)
+            {
+                rand = Random.Range(-12f, 12f);
+                Instantiate(snow, new Vector2(rand, 25), transform.rotation);
+            }
+        }
+    }
+}
