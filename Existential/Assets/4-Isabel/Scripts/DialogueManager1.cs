169c739affb8598913f2d64f368a58ab80c53882
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-17 16:55:11 -0700

    Updating level

---
+++ Existential/Assets/4-Isabel/Scripts/DialogueManager1.cs
@@ -0,0 +1,42 @@
+﻿// Created by Isabel
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager1 : MonoBehaviour{
+    public Text nameText;
+    public Text dialogueText;
+
+    private Queue <string> sentences;
+
+    // Start is called before the first frame update
+    void Start(){
+        sentences = new Queue <string>();
+    }
+
+    public void StartDialogue(Dialogue1 dialogue1){
+        nameText.text = dialogue1.name;
+        sentences.Clear();
+
+        foreach (string sentence in dialogue1.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(sentences.Count == 0){
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+
+    void EndDialogue(){
+        Debug.Log("End of conversation.");
+    }
+}
---
+++ Existential/Assets/4-Isabel/Scripts/DialogueTrigger1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger1 : MonoBehaviour{
+    public Dialogue1 dialogue1;
+    
+    public void TriggerDialogue(){
+        FindObjectOfType<DialogueManager1>().StartDialogue(dialogue1);
+    }
+}
