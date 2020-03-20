70aca86c810d6c98ee5170d8b295984f46c7dfb8
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-19 17:02:34 -0700

    Added BC mode to level

---
+++ Existential/Assets/4-Isabel/Scripts/IsabelHelp.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IsabelHelp : MonoBehaviour{
+    public void BCHelp (){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
