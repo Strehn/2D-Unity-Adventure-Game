57538a291b94f8441e97ffa7103b5c11ba688d59
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-20 18:06:12 -0700

    Updating level

--- Existential/Assets/4-Isabel/Scripts/LevelStuff.cs
+++ Existential/Assets/4-Isabel/Scripts/LevelStuff.cs
@@ -9,36 +9,53 @@ public class LevelStuff : MonoBehaviour{
     public GameObject Chalice;
     public GameObject Necklace;
     public GameObject Vase;
-    private Vector2 screenBounds;
+    public GameObject newObject;  // for Sam
 
     void Start(){
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
-        Instantiate(Chalice, new Vector3(0, -6.18f, 0), Quaternion.identity);
-
         int inventoryNumber = UnityEngine.Random.Range(1, 4);  // Generate a random inventory item to spawn on screen
-        Debug.Log(inventoryNumber);
+        // Debug.Log(inventoryNumber); testing purposes
 
         if(inventoryNumber == 1){
             // drop inventoryObject 1 on scene (belonging to Dia1)
             GameObject i = Instantiate(Chalice) as GameObject;
             i.SetActive(true);
             i.transform.position = new Vector2(0, -6.18f);
-
         }
         else if(inventoryNumber == 2){
             // drop inventoryObject 2 on scene (belonging to Dia2)
             GameObject i = Instantiate(Necklace) as GameObject;
             i.SetActive(true);
             i.transform.position = new Vector2(0, -6.18f);
-
         }
         else if(inventoryNumber == 3){
             // drop inventoryObject 3 on scene (belonging to Dia3)
             GameObject i = Instantiate(Vase) as GameObject;
             i.SetActive(true);
             i.transform.position = new Vector2(0, -6.18f);
-
         }
+        /* For Sam testing purposes:
+        else{
+            // drop random object on screen? This would initiate an error because in my scene I do 
+            // not have another gameObject and the number should be between 1-3 - so this could be a boundary test.
 
+            GameObject i = Instantiate(newObject) as GameObject;
+            i.SetActive(true);
+            i.transform.position = new Vector2(0, -6.18f);
+        }
+        */
+    }
+
+    void Update(){
+        /* For Sam testing purposes:
+            GameObject i = Instantiate(Chalice) as GameObject;
+            i.SetActive(true);
+            i.transform.position = new Vector2(0, -6.18f);
+        */
     }
 }
+
+/* For Sam: 
+    Boundary: Instantiating an object that doesn't exist
+    Boundary: Testing which speed it takes for the player to be able to run off screen through tilemap boundary
+    Stress: Instantiating too many objects on screen - the commented code in the update function
+*/
