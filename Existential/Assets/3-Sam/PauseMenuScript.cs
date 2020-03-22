9478fa1e781050b5a468c078e71e7782d8f06634
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-21 20:54:25 -0700

    Adding pause functionality to level

--- Existential/Assets/3-Sam/PauseMenuScript.cs
+++ Existential/Assets/3-Sam/PauseMenuScript.cs
@@ -1,51 +1,44 @@
-﻿using System.Collections;
+﻿// Created by Sam - updated by Isabel to fit coding standards and to use
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class PauseMenuScript : MonoBehaviour
-{
+public class PauseMenuScript : MonoBehaviour{
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
 
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (GameIsPaused)
-            {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if (GameIsPaused){
                 Resume();
             }
-            else
-            {
+            else{
                 Pause();
             }
         }
     }
 
-    public void Resume()
-    {
+    public void Resume(){
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
     }
 
-    void Pause()
-    {
+    public void Pause(){  // Changed to public for now by Isabel
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
 
-    public void LoadMenu(string scenename)
-    {
+    public void LoadMenu(string scenename){
         SceneManager.LoadScene(scenename);
     }
 
-    public void SaveGame()
-    {
+    public void SaveGame(){
 
     }
 }
