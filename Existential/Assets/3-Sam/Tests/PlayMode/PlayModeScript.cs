95f1a819c65bd07907dd01e8e0622861443ce832
samilynn13 <60374710+samilynn13@users.noreply.github.com>
2020-03-24 11:07:31 -0700

    Organized folders and added Test folder

---
+++ Existential/Assets/3-Sam/Tests/PlayMode/PlayModeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Tests
+{
+    public class PlayModeScript
+    {
+        
+        [UnityTest]
+        public IEnumerator Max_Inventory_Size()
+        {
+            SetUpScene("ToriScene");
+
+            yield return null;
+        }
+
+        public void SetUpScene(string scenename)
+        {
+            SceneManager.LoadScene(scenename);
+        }
+
+    }
+}
