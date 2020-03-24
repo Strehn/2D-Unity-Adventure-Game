d7de7a998aaa860044dc68215f3524d634450fa7
Victoria Gehring <victoria.gehring@gmail.com>
2020-03-23 20:12:57 -0700

    folder organization

---
+++ Existential/Assets/2-Tori/Scripts/FlowerInvItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerInvItem : MonoBehaviour, IInventoryItem
+{
+    public string Name{
+        get{
+            return "Flower";
+        }
+    }
+    public Sprite _Image = null;
+
+    public Sprite Image{
+        get{
+            return _Image;
+        }
+    }
+
+    public void OnPickup(){
+        gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+}
