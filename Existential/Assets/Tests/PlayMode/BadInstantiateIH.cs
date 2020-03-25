407d5b413d055220575e3232b5a08e27a2d3f403
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-25 15:50:50 -0700

    Updating tests

---
+++ Existential/Assets/Tests/PlayMode/BadInstantiateIH.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests{
+    public class BadInstantiateIH{
+        // Written by Isabel - used code from Team Lead 3's presentation
+        // Boundary test to instantiate an object that does not exist in the prefabs
+        [Test]
+        public IEnumerator InstantiateNonExistentObject(){
+            SetupScene();
+                if (GameObject.Find("newObject").GetComponent<Rigidbody2D>()){ 
+                    yield break;
+                }
+                else{
+                    Assert.Fail();
+                }
+        }
+
+        void SetupScene(){
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Canvas"));
+            MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/newObject"));
+        }
+    }
+}
+
+
+
