80bbbc643a27df0c2528bfc9464a64be6b6e3684
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-03-24 19:38:11 -0700

    Adding test cases

--- Existential/Assets/Tests/PlayMode/PlayModeScript.cs
+++ Existential/Assets/Tests/PlayMode/PlayModeScript.cs
@@ -6,21 +6,17 @@ using UnityEngine.TestTools;
 using UnityEngine.SceneManagement;
 using System;
 
-namespace Tests
-{
-    public class PlayModeScript
-    {
+namespace Tests{
+    public class PlayModeScript{
         //Written by Sam Spalding
         //Test to check to see if the game object "MainCharacter" is present in each scene
         //This may actually be a test for Isabel since she's the own in charge of the main character, but I don't know anymore.
         [Test]
-        public void Check_For_Main_Character_In_Scene_Test()
-        {
+        public void Check_For_Main_Character_In_Scene_Test(){
             var MC = GameObject.Find("MainCharacter"); //create a variable that holds a gameobject call "MainCharacter"
 
             //for each scene index in the Build Settings
-            for (int i = 1; i < 8; i++)
-            {
+            for (int i = 1; i < 8; i++){
                 SceneManager.LoadScene(i); //Load the scene indicated by the scene index
                 string FailOutput = "Main Character not found in scene index " + i + "."; //an output for which scene doesn't have a "MainCharacter" game object
                 Assert.IsNotNull(MC, FailOutput); //Check to see that variable MC "is not null" (or holds *something* where that something should be the MainCharacter).
