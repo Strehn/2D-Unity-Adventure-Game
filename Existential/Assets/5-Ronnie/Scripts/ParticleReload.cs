fd804a221d59b10f41c650e624fa86e049636ec3
Ronald Keating <rvnnie@yahoo.com>
2020-03-28 17:27:32 -0700

    Added Snow/Leaves particle effect

---
+++ Existential/Assets/5-Ronnie/Scripts/ParticleReload.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleReload : MonoBehaviour
+{
+    public GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (obj.transform.position.x < Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x)
+            obj.transform.position = new Vector2(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x, obj.transform.position.y);
+        else if (obj.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x)
+            obj.transform.position = new Vector2(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, obj.transform.position.y);
+    }
+}
