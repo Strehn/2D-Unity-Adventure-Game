a9cd1ff4f2317a9c3becc8dc7c055f5d18c3f703
Ronald Keating <rvnnie@yahoo.com>
2020-03-30 14:39:30 -0700

    Added Ronnie Test cases

--- Existential/Assets/5-Ronnie/Scripts/PlayerController2D.cs
+++ Existential/Assets/5-Ronnie/Scripts/PlayerController2D.cs
@@ -2,60 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController2D : MonoBehaviour
-{
+public class PlayerController2D : MonoBehaviour {
     Rigidbody2D rb;
     private float speed;
 
+    //determine if player is touching ground, do not want double jumps
     bool isGround;
 
-    [SerializeField]
-    Transform groundCheck;
+    [SerializeField] Transform groundCheck;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rb = GetComponent<Rigidbody2D>();
         speed = 4f;
     }
 
-    private void FixedUpdate()
-    {
-        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground")))
-        {
+    private void FixedUpdate() {
+        if (Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"))) {
             isGround = true;
         }
-        else
-        {
+        else {
             isGround = false;
         }
 
-        if (Input.GetKey("right"))
-        {
+        if (Input.GetKey("right")) {
             rb.velocity = new Vector2(1 * speed, rb.velocity.y);
         }
-        else if (Input.GetKey("left"))
-        {
+        else if (Input.GetKey("left")) {
             rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
         }
-        else
-        {
+        else {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
 
-        if(Input.GetKey("up") && rb.position.x >= 91.5 && rb.position.x <= 92.5)
-        {
+        if(Input.GetKey("up") && rb.position.x >= 91.5 && rb.position.x <= 92.5) {
             //temp respawn to beginning of level, will add more.
             transform.position = new Vector2(-8, -1);
         }
-        else if (Input.GetKey("up") && isGround)
-        {
+        else if (Input.GetKey("up") && isGround) {
             rb.velocity = new Vector2(rb.velocity.x, 9.5f);
         }
 
-        if(rb.position.y < -3f)
-        {
+        if(rb.position.y < -3f) {
             transform.position = new Vector2(-8, -1);
         }
     }
