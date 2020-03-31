7ea924d42f6269c8bde948905427e0ef208a7d67
Sydney Petrehn <Sydneypetrehn!@gmail.com>
2020-03-30 23:08:51 -0600

    Stress Test Working

--- Existential/Assets/Tests/PlayMode/SydneyTests.cs
+++ Existential/Assets/Tests/PlayMode/SydneyTests.cs
@@ -3,78 +3,63 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests
-{
+namespace Tests{
     /* Sydney Petrehn Tests
      * 1; Stress Test: Spawn 100 bunnies and see if frame rate drops unreasonably low
      * 2: Bounds Test 1: Using moving Bunny against stationary Trees
      * 3: Bounds Test 2: Using moving Tree against stationary Bunnies
      */
 
-    public class SydneyTests
-    {
+    public class SydneyTests{
 
         // Stress Test
         // [PASS] The frame rate does not drop unreasonable low.
         // [FAIL] The frame rate does drop unreasonably low.
         [UnityTest]
-        public IEnumerator Bunny_StressTest()
-        {
-            // Amount of Bunnies to spawn / spawned
-            int i = 0;
+        public IEnumerator Bunny_StressTest(){
+            // counter for Amount of Bunnies to spawn / spawned
+            int count = 1; //Scene starts with one in the scene
             // spawn location
-            Vector2 StartLocation = new Vector2(2, 1);
+            Vector2 SpawnLocation = new Vector2(2, 1);
+            // Used to calculate frame rate
+            var deltaTime = 0.0;
+            var fps = 0.0;
+            var timeBegin = 0.0;
             //Instantiates the Objects
             SetupScene();
 
-            //Track time it is taking
-            var StartTime = 1 / Time.deltaTime;
-
+            //Begin Stress Testing
             //Instantiate a ton of bunnies
-            for (i = 0; i < 100; i++)
-            {
-                //SpawnBunny multiple times
-                MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/SpawnedBunny"));
+            for (int i = 0; i < 100; i++){
+                //Spawn a new Bunny
+                Spawner(SpawnLocation);
+                count++;
 
-            }
+                timeBegin += Time.deltaTime;
 
-            // Get End Time
-            var EndTime = 1 / Time.deltaTime;
+                //calcuate FPS
+                deltaTime += Time.deltaTime;
+                deltaTime /= 2.0;
+                fps = 1.0 / deltaTime;
+                Debug.Log("Frames: " + fps);
 
-            var TTime = EndTime - StartTime;
-
-            // Give Time
-            Debug.Log(TTime);
-
-            //Let the test run for 3 secconds
-            yield return new WaitForSeconds(3);
-
-
-            // Do Frames per second here instead
-            //
-            //
-            // See how it affected the game time
-            if (TTime < 10)
-            {
-                Debug.Log((i + 1) * 100);
-                if (i < 100)
-                {
+                // If frames drop below 10, stop the test
+                if (fps < 10){
+                    var timeFinish = timeBegin % 60;
                     Assert.Fail();
                 }
-                Assert.Pass();
-                yield break;
+                yield return new WaitForSeconds(0.1f);
             }
-            //
-            //
-
+            Assert.Pass();
         }
+
+
 
         // Bounds Test 1
         // [PASS] The Bunny is does not go through the boundary of trees.
         // [FAIL] The Bunny goes through the boundary of trees.
         [UnityTest]
-        public IEnumerator Bunny_BoundsTest()
-        {
+        public IEnumerator Bunny_BoundsTest(){
             //Move the bunnies super fast
             int currentSpeed = 10;
             // spawn location
@@ -90,8 +75,7 @@ namespace Tests
             Rigidbody2D rb = SpawnedBunny.GetComponent<Rigidbody2D>();
 
             //Runs the test for a total of 10 runs, if it does not break the wall, the test passes
-            for (int i = 0; i < 10; i++)
-            {
+            for (int i = 0; i < 10; i++){
                 //Function to give the Bunny velocity and reset if it hits the wall
                 Bunny_Move(currentSpeed, StartLocation, SpawnedBunny, rb);
 
@@ -101,13 +85,11 @@ namespace Tests
                 //Function to check if the bunny is out of bounds
                 bool returnedValue = Check_Position(SpawnedBunny);
 
-                if (returnedValue == false)
-                {
+                if (returnedValue == false){
                     currentSpeed += 1;
                     Debug.Log("Adding Speed - Current Speed: " + currentSpeed);
                 }
-                else
-                {
+                else{
                     Debug.Log("Asserting False - Final Speed: " + currentSpeed);
                     Assert.Fail();
                 }
@@ -120,8 +102,7 @@ namespace Tests
         // [PASS] The tree does not go through the boudnary of bunnies.
         // [FAIL] The tree goes though the boundary of bunnies.
         [UnityTest]
-        public IEnumerator treeMoving_BoundsTest()
-        {
+        public IEnumerator treeMoving_BoundsTest(){
             //Move the tree speedy fast
             int currentSpeed = 80;
             //Dont move the bunnies
@@ -149,22 +130,19 @@ namespace Tests
 
 
         //Function to setup the scene
-        void SetupScene()
-        {
-            //Prefab with everything needed for This bounds test
+        void SetupScene(){
+            //Prefab with everything needed for Stress test and bounds test 1
             MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TestingForestTileMap"));
         }
 
         //Function to setup the scene for Tree Moving
-        void SetupSceneTree()
-        {
-            //Prefab with everything needed for This bounds test
+        void SetupSceneTree(){
+            //Prefab with everything needed for bounds test 2
             MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/TestingBunnyTileMap"));
         }
 
         //Function to give the Bunny Velocity and reset the Bunny
-        void Bunny_Move(int currentSpeed, Vector2 StartLocation, GameObject SpawnedBunny, Rigidbody2D rb)
-        {
+        void Bunny_Move(int currentSpeed, Vector2 StartLocation, GameObject SpawnedBunny, Rigidbody2D rb){
             //Debug.Log("Moving Bunny");
             if (SpawnedBunny.transform.position.x > 1f && SpawnedBunny.transform.position.x < 100f)
             {
@@ -174,15 +152,18 @@ namespace Tests
             rb.velocity = new Vector2(currentSpeed, 0);
         }
 
+        //Functin to spawn bunnies
+        void Spawner(Vector2 SpawnLocation){
+            GameObject bunny = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/SpawnedBunny"));
+            bunny.transform.position = SpawnLocation;
+        }
+
         //Function to check if the Bunny is past the wall
-        bool Check_Position(GameObject SpawnedBunny)
-        {
-            if (SpawnedBunny.transform.position.x > 100)
-            {
+        bool Check_Position(GameObject SpawnedBunny){
+            if (SpawnedBunny.transform.position.x > 100){
                 return true;
             }
-            else
-            {
+            else{
                 return false;
             }
         }
