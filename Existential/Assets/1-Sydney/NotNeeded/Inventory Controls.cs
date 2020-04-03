4b029df3d11290e61264af0e2e1c627c9ee09243
Taegan Williams <taeganw@gmail.com>
2020-04-02 19:55:32 -0700

    Revert "adding in gamefiles"
    This reverts commit 31707ffae094157d82c710589ccad4339ac56894.

---
+++ Existential/Assets/1-Sydney/NotNeeded/Inventory Controls.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    This script is used in the game to open the inventory. When the player clicks 
+    On the inventory button then the inventory will open. 
+*/
+
+public class InventoryControls : MonoBehaviour{
+    // Start is called before the first frame update
+    void Start(){
+        //Open Inventory Scene
+    }
+
+}
+
