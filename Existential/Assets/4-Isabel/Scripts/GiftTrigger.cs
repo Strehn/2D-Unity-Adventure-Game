e7548b43d06331c0733adb9067121f1a9f950d68
hink0402 <60370419+hink0402@users.noreply.github.com>
2020-04-16 13:19:18 -0700

    Working on gift interaction

---
+++ Existential/Assets/4-Isabel/Scripts/GiftTrigger.cs
@@ -0,0 +1,26 @@
+﻿/* Script created by Isabel Hinkle to trigger a gift exchange when two characters collide
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GiftTrigger : MonoBehaviour{
+
+    public PlayerControllerRigidBody mainCharacter;
+    public GameObject NPC1;
+    public GameObject NPC2;
+    public GameObject NPC3;
+    public Button giftButton;
+
+    void Start(){
+        mainCharacter = FindObjectOfType<PlayerControllerRigidBody>();
+    }
+
+    public void OnTriggerEnter(Collider other ){
+        if(other.gameObject.tag == "interObject"){
+            Debug.Log("Ran into NPC");
+            giftButton.gameObject.SetActive(true);
+        }
+    }
+}
