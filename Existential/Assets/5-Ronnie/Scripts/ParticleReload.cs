42d4d1f3cbd43cee7703cabdf4a04e13407074e7
Ronald Keating <rvnnie@yahoo.com>
2020-04-21 19:55:39 -0700

    Fixed snow particle

--- Existential/Assets/5-Ronnie/Scripts/ParticleReload.cs
+++ Existential/Assets/5-Ronnie/Scripts/ParticleReload.cs
@@ -4,22 +4,34 @@ using UnityEngine;
 
 public class ParticleReload : MonoBehaviour {
     private GameObject obj;
+    private Rigidbody2D rigidbody;
     public float bottomOfLevel;
+    public float speed;
     // Start is called before the first frame update
     void Start() {
         obj = this.gameObject;
-        bottomOfLevel = -3f;
+        rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = new Vector2(0, speed);
+        //bottomOfLevel = Camera.main.ScreenToWorldPoint(new Vector2(0,0).y);
     }
 
     // Update is called once per frame
     void Update() {
-        if (obj.transform.position.y < bottomOfLevel)
-            Destroy(obj);
+        /*if (obj.transform.position.y < bottomOfLevel)
+            Destroy(obj);*/
         if (obj != null) {
             if (obj.transform.position.x < Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x)
                 obj.transform.position = new Vector2(Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x, obj.transform.position.y);
             else if (obj.transform.position.x > Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x)
                 obj.transform.position = new Vector2(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, obj.transform.position.y);
+        }
+        if(obj != null) {
+            if (obj.transform.position.y < Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y) {
+                obj.transform.position = new Vector2(obj.transform.position.x, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+                //rigidbody.velocity = new Vector2(0, -10);
+            }
+            else if (obj.transform.position.y > Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y)
+                obj.transform.position = new Vector2(obj.transform.position.x, Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y);
         }
     }
 }
