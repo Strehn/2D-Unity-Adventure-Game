be0a086616d9cf90a969ae93e13ae4e6d07743b2
samilynn13 <60374710+samilynn13@users.noreply.github.com>
2020-04-23 15:02:28 -0700

    Made Changes to Pause menu and my level

---
+++ Existential/Assets/3-Sam/Scripts/NewLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewLevel : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SceneManager.LoadScene("4-Isabel/Scenes/Isabel Level");
+        }
+    }
+}
