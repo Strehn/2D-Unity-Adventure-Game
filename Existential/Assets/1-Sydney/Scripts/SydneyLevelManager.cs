7ae4c74521d90d8c57cbd076fbccb9ff61b17e91
Sydney Petrehn <Sydneypetrehn!@gmail.com>
2020-04-26 14:31:51 -0600

    Dialog Editing

--- Existential/Assets/1-Sydney/Scripts/SydneyLevelManager.cs
+++ Existential/Assets/1-Sydney/Scripts/SydneyLevelManager.cs
@@ -11,27 +11,29 @@ public class SydneyLevelManager : MonoBehaviour
     // Need this to know how many inventory items i have
     //Potential here for pair programming: Get the number of inventory items from the inventory to actually implement this
     public int inventoryList = 4;
-    public GameObject mainCharacter;
-
+    public GameObject mainChar = GameObject.Find("MainCharacter");
 
     // Handles scene transitions for Forest to NightForest
     // Handles scene transitions for NightForest to next Scene
     public void Update()
     {
         Scene scene;
-        Transform transform = mainCharacter.GetComponent<Transform>();
+        Transform transform = mainChar.GetComponent<Transform>();
         Vector2 position = transform.position;
         scene = SceneManager.GetActiveScene();
         if (scene.buildIndex == 3)
         {
-            if ((position.x >= 19) && (position.y <= 16) && (inventoryList == 4))
+            if ((position.x <= 19) && (position.y >= 16.5) && (inventoryList == 4))
             {
                 SceneManager.LoadScene(8);
             }
         }
         if (scene.buildIndex == 8)
         {
-            if (position.x >= 13 && position.y >= 11.4)
+            mainChar = GameObject.Find("PlayerCharacter");
+            transform = mainChar.GetComponent<Transform>();
+            position = transform.position;
+            if (position.x >= 13 && position.y <= 6.9)
             {
                 SceneManager.LoadScene(5);
             }
