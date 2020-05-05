b03e9003eb719c254cc51c8761aa7d05ded5446b
Taegan Williams <taeganw@gmail.com>
2020-05-05 10:56:03 -0700

    added End Credit Scene and fixed audio

---
+++ Existential/Assets/6-Taegan/scripts/ScrollUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScrollUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float positiony;
+    float positionx;
+    // Update is called once per frame
+    void Update()
+    {
+        positiony = this.GetComponent<RectTransform>().position.y;
+        positionx = this.GetComponent<RectTransform>().position.x;
+        this.GetComponent<RectTransform>().position = new Vector2(positionx, positiony + 1f);
+        if (positiony > 2000)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
