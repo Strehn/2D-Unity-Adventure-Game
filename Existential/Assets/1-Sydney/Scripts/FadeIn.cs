9ec031ff476415b0099b54ad0daa03a1f84e0e43
Sydney Petrehn <Sydneypetrehn!@gmail.com>
2020-05-05 12:20:13 -0600

    Pair Programming

---
+++ Existential/Assets/1-Sydney/Scripts/FadeIn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIn : MonoBehaviour
+{
+
+	public void Fadeobject()
+	{
+		GameObject spriteRenderer = GameObject.Find("Tutorial");
+		float AlphaValue = 1.0f;
+		int FadeType = 0;
+	
+		// fade in 
+		if (FadeType == 0)
+		{
+			AlphaValue -= 0.25f * Time.deltaTime;
+			// limit the possible alpha value
+			if (AlphaValue < 0.0f)
+			{
+				AlphaValue = 0.0f;
+			}
+		}
+
+		spriteRenderer.SetActive(false);
+	}
+
+}
